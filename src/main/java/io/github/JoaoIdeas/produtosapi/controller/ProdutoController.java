6019b5406a299319963cc32e9356ab7de2cc78c0
Joao Victor <joaovictor0802@hotmail.com>
2025-07-04 19:01:53 -0300

    Primeiros Passos com Spring Boot

--- src/main/java/io/github/JoaoIdeas/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/JoaoIdeas/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import io.github.JoaoIdeas.produtosapi.model.Produto;
 import io.github.JoaoIdeas.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -34,5 +35,17 @@ public class ProdutoController {
     @DeleteMapping("{id}")
     public void deletar(@PathVariable("id") String id){
         produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id,
+                          @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
     }
 }
